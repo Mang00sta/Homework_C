ccdcdc55dbda07f2ace6ee13bd210cf95b44ac43
Vishnyakov <mangostalife@gmail.com>
2022-06-24 17:15:57 +0300

    DZ_2 complete. сделал через else if

--- Homework_1/DZ_2/Program.cs
+++ Homework_1/DZ_2/Program.cs
@@ -1,2 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача №4
+
+/*
+Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+2, 3, 7 -> 7
+44 5 78 -> 78
+22 3 9 -> 22
+*/
+
+int a, b, c;
+
+Console.Write("Insert first number =  ");
+a=Convert.ToInt32(Console.ReadLine());
+Console.Write("Insert second number =  ");
+b=Convert.ToInt32(Console.ReadLine());
+Console.Write("Insert third number =  ");
+c=Convert.ToInt32(Console.ReadLine());
+
+if(a>b)
+{
+Console.WriteLine("max: " + a);
+}
+else if(b>c)
+{
+Console.WriteLine("max:" + b);
+}
+else if (c>a)
+{
+Console.WriteLine("max:" + c);
+}
+
+if(a==b)
+{
+Console.WriteLine("Identically: " + a);
+}
+else if(b==c)
+{
+Console.WriteLine("Identically: " + b);
+}
+else if (c==a)
+{
+Console.WriteLine("Identically: " + c);
+}
+

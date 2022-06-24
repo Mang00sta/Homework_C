be4cbaf65d6e38915dd980fb2fcccf513f38fa2a
Vishnyakov <mangostalife@gmail.com>
2022-06-24 21:12:10 +0300

    DZ_4 complete. Возникла сложность  с вставкой в цикл if. И вставкой %2==0. С синтаксисом проблема пока и структурой написания кода.

--- Homework_1/DZ_4/Program.cs
+++ Homework_1/DZ_4/Program.cs
@@ -1,1 +1,36 @@
-﻿
+﻿// Задача №8
+
+/*
+Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N. 
+5 -> 2,4
+8 -> 2, 4, 6, 8
+*/
+
+int n, current=0;
+
+Console.Write("input a num ");
+n = Convert.ToInt32(Console.ReadLine());
+
+while(current <= n)
+{
+        if (current % 2 == 0) 
+        Console.Write(current + " ");
+        current++; 
+}
+
+//Второй вариант решения 
+/*
+int n, current=0;
+
+Console.Write("input a number: ");
+n = Convert.ToInt32(Console.ReadLine());
+
+
+while(current <= n)
+{
+    Console.Write(current + " ");
+    current+=2; 
+}    
+*/
+
+

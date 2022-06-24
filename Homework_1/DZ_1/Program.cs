3d5239e0643946a4c129a28739434741aed95140
Vishnyakov <mangostalife@gmail.com>
2022-06-24 15:58:14 +0300

    DZ_1 complete. внес дополнение при равенстве значений

--- Homework_1/DZ_1/Program.cs
+++ Homework_1/DZ_1/Program.cs
@@ -1,1 +1,34 @@
-﻿
+﻿// Задача №1
+/* 
+Напишите программу, которая на вход принимает два числа и выдаёт, 
+какое число большее, а какое меньшее. 
+a = 5; b = 7 -> max = 7
+a = 2 b = 10 -> max = 10
+a = -9 b = -3 -> max = -3
+*/
+
+int a, b;
+
+Console.Write("Insert first number =  ");
+a=Convert.ToInt32(Console.ReadLine());
+Console.Write("Insert second number =  ");
+b=Convert.ToInt32(Console.ReadLine());
+
+
+if(a==b)
+{
+        Console.Write("Identically");
+        System.Environment.Exit(0);
+} 
+
+if(a>b) 
+{
+Console.WriteLine("max:" + a);
+Console.Write("min:" + b);
+}
+else
+{
+Console.WriteLine("max:" + b);
+Console.Write("min:" + a);
+}
+

42ce53e97df46480950528762d8424ddb12652be
Vishnyakov <mangostalife@gmail.com>
2022-07-06 01:43:46 +0300

    Сделал 2ю задачу

--- Homework_3/Program.cs
+++ Homework_3/Program.cs
@@ -2,27 +2,50 @@
 //14212 -> нет
 //12821 -> да
 //23432 -> да
+/*
+void Polindrom(int num)
+ {  
+    int temp = num, sum=0, rem=0; // ввели переменные
+    for (; temp>sum; ) // нужно было while, no я решил через for попробовать.  
+        {                       // 75457
+            rem = num % 10;  // нашли последнее число 7-5-4-5-4-7
+            num = num / 10;  // нашли остальное число 7545-754-75-7
+            sum = sum * 10 + rem;  // нашли число в обратном порядке .... 7-75-754-7545-75457 
+        }
+    if (temp == sum) Console.WriteLine (" Number is Palindrome" ); // сравнили с исходным 
+    if (temp != sum) Console.WriteLine (" NO " ); // неравен с исходным
+}
 
-//*
-void Polindrom(int n)
+Console.WriteLine("Insert n "  );
+int n = Convert.ToInt32(Console.ReadLine());
+
+Polindrom(n);
+
+Console.ReadKey();
+Console.Clear();
+/*/
+
+//! Решение через string не удалось в виду малых знаний написания кода. Хотелось бы быстро разобрать такой вариант решения на семинаре. 
+
+/*
+string s = "123456"; //не удалось начало ввод
+
+string Polindrom(string s)
 {
-    int i=0;
-    int j=0;
-    for (j=0; j<=i/2; j++)
-        for (i=0; i<=i-1; i--)
-            if (j!=i-1)
+     int length = s.Length;
+     for (int i = 0; i < length / 2; ++i)
+        if (s[i] != s[length - 1 - i])
             {
-                Console.WriteLine("No");
+                return Console.WriteLine("1");
+            }
+        else 
+            {
+                return Console.WriteLine("2");
             }
 }
-Console.WriteLine("Insert n "  );
-int num = Convert.ToInt32(Console.ReadLine());
 
-Polindrom(num);
-//Console.ReadKey();
-//Console.Clear();
-//*/
-
+Console.WriteLine(Polindrom(s));
+*/
 
 //! Задача 23 Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N. 
 // 3 -> 1, 8, 27
@@ -44,4 +67,4 @@ CubeN(num);
 
 Console.ReadKey();
 Console.Clear();
-//*/
+/*/

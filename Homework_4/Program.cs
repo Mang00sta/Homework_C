b20a5d9fd4a3db8b71c3f9df70d2781fac5fef4b
Vishnyakov <mangostalife@gmail.com>
2022-07-07 09:47:13 +0300

    Задача 27 решена

--- Homework_4/Program.cs
+++ Homework_4/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт the sum of the numbers among.
+//452 -> 11
+//82 -> 10
+//9012 -> 12
+
+int Dig (int n)
+{
+     int temp = n, rem=0, sum=0; 
+    for (; n>0 ;   ) 
+        {                       // 75457
+            rem = n % 10; 
+            n = n / 10; // нашли последнее число 7-5-4-5-4-7
+            sum=sum+rem;
+           // нашли число в обратном порядке .... 7-75-754-7545-75457 
+  
+        }
+        return sum;
+
+}
+
+Console.Write ("Insert number: ");
+int num=Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("The sum og the numbers among: " + Dig(num));
+

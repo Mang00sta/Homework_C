594cd720d2304bfba5b599a98a69eecc6b1cdad2
Vishnyakov <mangostalife@gmail.com>
2022-07-14 03:48:00 +0300

    Сделал задачу 41 в 2х вариантах

--- Homework_6/Program.cs
+++ Homework_6/Program.cs
@@ -1,2 +1,79 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//!Задача 41: 
+//Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+//0, 7, 8, -2, -2 -> 2
+//1, -7, 567, 89, 223-> 3
+/*
+int [] CreateArray (int size) 
+{
+    int[] array = new int [size];
+
+    Console.WriteLine("Creating array: ");
+    for (int i=0; i<size; i++)
+    {
+        {
+            Console.WriteLine($"Input {i+1} element: "); 
+            array[i] = Convert.ToInt32(Console.ReadLine());   
+        }
+    }
+    Console.WriteLine();
+    return array;
+}
+void ShowArray (int[] array) 
+{
+    for (int i=0; i<array.Length; i++) 
+    Console.Write(array[i] + " ");
+    
+}
+int HowDigit (int [] array)
+    {
+        int count = 0;
+        for (int i=0;i<array.Length; i++)
+            if (array[i]>0) count++; 
+        
+        return count;
+        
+    }
+    
+Console.Write("Input array: ");
+int num=Convert.ToInt32(Console.ReadLine()); Console.WriteLine();
+
+int [] array = CreateArray (num);
+Console.WriteLine ($"Array: "); ShowArray(array); 
+Console.WriteLine();
+int array2 = HowDigit (array);
+Console.WriteLine($"Numbers more thеrе '0': {array2}"); 
+/*/
+
+// Вариант по моему мнению более правильный под определение "Пользователь вводит с клавиатуры M чисел". Нарыл в stack overflow как ввести массив пользователю цифрами подряд и конвертнуть в int32.    
+/*
+int HowDigit (int [] array)
+    {
+        int count = 0;
+        for (int i=0;i<array.Length; i++)
+            if (array[i]>0) count++; 
+        
+        return count;
+        
+    }
+ 
+Console.Write("Input array from space: ");
+int[] num = Array.ConvertAll(Console.ReadLine().Split(), int.Parse); 
+// создаем массив который забирает string от пользователя через пробел Split и конвертим в int 32 с помощью Parse. Ну и convertAll переводит в массиве из одного типа в другой. Единственное не смог сделать это как то раздельно, что бы отработал split -> метод конвертирования массива в численное -> метод поиска сколько чисел больше 0. т.е. приближено к решению выше. Ради интереса. Так то этот вариант лучше и на 1 метод меньше.    
+
+Console.WriteLine();
+int array2 = HowDigit (num);
+Console.WriteLine($"Numbers more thеrе '0': {array2}"); 
+/*/
+
+//! Задача 43: 
+//Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+//b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+
+
+
+
+
+
+

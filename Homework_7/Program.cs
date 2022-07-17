c68faeddf862bedda7803e86658e0ecd620e2bc6
Vishnyakov <mangostalife@gmail.com>
2022-07-17 21:45:14 +0300

    Задача 47 выполнена

--- Homework_7/Program.cs
+++ Homework_7/Program.cs
@@ -1,2 +1,62 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Методы
+double [,] CreateRandom2DArrayMaterialNum (int rows, int columns)
+{
+    double [,] newArray =new double[rows, columns];
+    Random rnd = new Random();
+    for (int i = 0; i<rows; i++) // цикл 1
+            for (int j=0; j<columns; j++ ) // цикл 2
+            {
+              newArray[i,j] = Math.Round(rnd.NextDouble() * (100 - (-100)) + (-100),2); // округлил до сотых через Math.Round. Сделал числа через NextDouble и конверитировал их до десятых.   Как сделать что бы и сотые были и целые я не знаю!         
+            }
+    
+        return newArray;
+}
+void Show2DArray (double [,] array)
+{
+    for (int i =0; i<array.GetLength(0); i++) // 0??
+    {
+        for (int j=0; j<array.GetLength(1); j++) //??
+        //Console.Write("{0,6:F2}", array[i,j]);
+       Console.Write(array[i,j] + "\t");
+
+        Console.WriteLine(); // ставиться только во внутри 2 цикла
+    }
+}
+
+//! Задача 47. 
+//Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+/*
+Console.WriteLine("Input number of rows: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input number of columns: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+double [,] RandomArrayMaterialNum = CreateRandom2DArrayMaterialNum(m,n);
+Show2DArray(RandomArrayMaterialNum);
+/*/
+
+
+//!Задача 50. 
+//Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+//Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 17 -> такого числа в массиве нет
+
+
+
+
+//! Задача 52. 
+//Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+

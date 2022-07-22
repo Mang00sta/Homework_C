6ba1fe782616c675267befae24ce0c3aabc71d95
Vishnyakov <mangostalife@gmail.com>
2022-07-22 03:44:30 +0300

    решил 66 задачу

--- Homework_9/Program.cs
+++ Homework_9/Program.cs
@@ -1,25 +1,36 @@
 ﻿//!Задача 64:
 // Задайте значение N. Напишите программу, которая найдет кол-во цифр в числе N рекурсивным методом.
 //N = 4532 -> 4
-
+/*
 int NumDigi(int n)
 {
     int count = 1;
     if (n!=0)
     {
-        if (n / 10 != 0)
-           count+= NumDigi(n / 10);
+    if (n / 10 != 0)
+        count+= NumDigi(n / 10);
     }
     return count;
 }
 
 Console.WriteLine(NumDigi(851)); 
-
-
-
+/*/
 
 //!Задача 66:
 // Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
 //M = 1; N = 15 -> 120 
 //M = 4; N = 8. -> 30
 
+int SumofDigi (int m, int n)
+{
+   
+    if (n > m) 
+    {
+   
+       return   n + SumofDigi(m,n-1);
+       
+    }
+    return n;
+}
+
+Console.WriteLine(SumofDigi(4,8));
